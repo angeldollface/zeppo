04ff913b97e684ed09953b02acef76ef467c2fae
angeldollface <angeldollface666@gmail.com>
2022-09-11 16:06:59 +0200

    Init.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/*
+ZEPPO.JS by Alexander Abraham, a.k.a. "Angel Dollface".
+Licensed under the MIT license.
+*/
+
+'use strict';
+
+// Importing all functions from './zeppo.js'.
+import {
+    decToBin,
+    binToDec,
+    reverseArray,
+    letterIndex,
+    getLetterFromIndex,
+    hexToDec,
+    isBin,
+    isInt,
+    testAll
+} from './zeppo.js';
+
+// Exporting all functions.
+export default {
+    decToBin,
+    binToDec,
+    reverseArray,
+    letterIndex,
+    getLetterFromIndex,
+    hexToDec,
+    isBin,
+    isInt,
+    testAll
+};
